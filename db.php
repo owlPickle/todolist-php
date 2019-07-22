7268508f8e8acd0c8dfae8c8f5f27de568eebaaf
owlPickle <terry998877terry@gmail.com>
2019-07-22 19:46:44 +0800

    delete & ignore db.php

--- db.php
+++
@@ -1,9 +0,0 @@
-<?php 
-$db = [
-    'host' => 'localhost',
-    'dbname' => 'newtodolist_demo',
-    'port' => 8889,
-    'charset' => 'utf8',
-    'username' => 'biker',
-    'password' => 1234,
-];
